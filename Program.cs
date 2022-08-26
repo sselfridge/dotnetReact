42ca1c23ccf93a5680151933db75f0b80bc0f222
Sam Selfridge <sam.selfridge@gmail.com>
2022-08-26 15:47:31 -0700

    useUrls

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.WebHost.UseUrls("http://*:5000", "https://*:5001");
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
@@ -9,8 +9,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +22,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");;
+app.MapFallbackToFile("index.html");
 
 app.Run();
